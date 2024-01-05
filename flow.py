93726a6dda81c1e8ad846ea91e38f1f91a8168af
kanatatsu64 <kanatatsu64@gmail.com>
2024-01-05 16:57:15 +0900

    startを修正

--- flow.py
+++ flow.py
@@ -1,32 +1,52 @@
 import sys
 import subprocess
 
-class Flow:
-    def __init__(self, exec):
-        self.exec = exec
+def exec(command):
+    print(command)
+    return subprocess.run(command, capture_output=True, text=True, shell=True).stdout
 
-    def start(self, flow_name, base_branch = None):
+def usage():
+    print("flow init <flow_name> [<base_branch>]")
+    print("flow start <flow_name>")
+    print("flow checkout <feature_name>")
+    print("flow list")
+    print("flow push")
+    print("flow rebase")
+    sys.exit(1)
+
+class Flow:
+    def __init__(self, exec, print):
+        self.exec = exec
+        self.print = print
+
+    def init(self, flow_name, base_branch = None):
         if base_branch is None:
             base_branch = f"develop/{flow_name}"
         
         if not self.exists(base_branch):
-            raise "base branch not found"
+            self.exec(f"git checkout -b {base_branch}")
         
-        parsed_flow_name = flow_name.replace("/", "_")
+        self.set_flow_name(flow_name)
+        self.set_base_branch(flow_name, base_branch)
 
-        self.exec(f"mkdir -p .git/flow")
-        self.exec(f"echo \"{flow_name}\" > .git/flow_current")
-        self.exec(f"echo \"{base_branch}\" > .git/flow/{parsed_flow_name}")
+    def start(self, flow_name):
+        base_branch = self.get_base_branch(flow_name)
+
+        if base_branch == "":
+            usage()
+
+        self.set_flow_name(flow_name)
+        self.checkout_base()
     
     def checkout(self, feature_name=None):
         if feature_name is None:
             self.checkout_base()
             return
 
-        self.exec(f"echo \"{feature_name}\" > .git/flow_current_feature")
+        self.set_feature_name(feature_name)
 
         flow_name = self.get_flow_name()
-        base_branch = self.get_base_branch()
+        base_branch = self.get_base_branch(flow_name)
         feature_branch = f"feature/{flow_name}/{feature_name}"
         exists = self.exists(feature_branch)
 
@@ -36,9 +56,10 @@ class Flow:
             self.exec(f"git checkout -b {feature_branch} {base_branch}")
     
     def checkout_base(self):
-        self.exec(f"echo \"\" > .git/flow_current_feature")
+        self.set_feature_name("")
 
-        base_branch = self.get_base_branch()
+        flow_name = self.get_flow_name()
+        base_branch = self.get_base_branch(flow_name)
 
         self.exec(f"git checkout {base_branch}")
     
@@ -47,7 +68,7 @@ class Flow:
         pattern = f"feature/{flow_name}/*"
         lines = self.exec(f"git branch --list '{pattern}' | tr '*' ' '").splitlines()
         for line in lines:
-            print(line.strip()[len(pattern)-1:])
+            self.print(line.strip()[len(pattern)-1:])
 
     def push(self):
         flow_name = self.get_flow_name()
@@ -55,7 +76,7 @@ class Flow:
         feature_branch = f"feature/{flow_name}/{feature_name}"
 
         if feature_name == "":
-            raise "not in feature branch"
+            usage()
     
         self.exec(f"git push -u origin {feature_branch}")
 
@@ -63,42 +84,66 @@ class Flow:
         return not (self.exec(f"git branch --list {branch}").strip() == "")
     
     def rebase(self):
-        base_branch = self.get_base_branch()
+        flow_name = self.get_flow_name()
+        base_branch = self.get_base_branch(flow_name)
         
         self.exec(f"git fetch origin {base_branch}")
         self.exec(f"git rebase origin/{base_branch}")
     
+    def set_flow_name(self, flow_name):
+        self.exec(f"echo \"{flow_name}\" > .git/flow_current")
+
     def get_flow_name(self):
         return self.exec("cat .git/flow_current").strip()
+
+    def set_base_branch(self, flow_name, base_branch):
+        parsed_flow_name = flow_name.replace("/", "_")
+        self.exec(f"mkdir -p .git/flow")
+        self.exec(f"echo \"{base_branch}\" > .git/flow/{parsed_flow_name}")
     
-    def get_base_branch(self):
-        flow_name = self.get_flow_name()
+    def get_base_branch(self, flow_name):
         parsed_flow_name = flow_name.replace("/", "_")
         return self.exec(f"cat .git/flow/{parsed_flow_name}").strip()
+
+    def set_feature_name(self, feature_name):
+        self.exec(f"echo \"{feature_name}\" > .git/flow_current_feature")
     
     def get_feature_name(self):
         return self.exec("cat .git/flow_current_feature").strip()
 
-def exec(command):
-    return subprocess.run(command, capture_output=True, text=True, shell=True).stdout
-
 if __name__ == '__main__':
-    flow = Flow(exec)
+    flow = Flow(exec, print)
     args = sys.argv[1:]
 
-    if args[0] == "start":
+    if len(args) == 0:
+        usage()
+
+    if args[0] == "init":
+        if len(args) == 2:
+            flow.init(args[1])
+            sys.exit(0)
+        elif len(args) == 3:
+            flow.init(args[1], args[2])
+            sys.exit(0)
+    elif args[0] == "start":
         if len(args) == 2:
             flow.start(args[1])
-        elif len(args) == 3:
-            flow.start(args[1], args[2])
+            sys.exit(0)
     elif args[0] == "checkout":
         if len(args) == 1:
             flow.checkout()
+            sys.exit(0)
         elif len(args) == 2:
             flow.checkout(args[1])
+            sys.exit(0)
     elif args[0] == "list":
         flow.list()
+        sys.exit(0)
     elif args[0] == "push":
         flow.push()
+        sys.exit(0)
     elif args[0] == "rebase":
         flow.rebase()
+        sys.exit(0)
+    
+    usage()
--- test.py
+++ test.py
@@ -22,14 +22,18 @@ class ProcessMock:
             if key == command:
                 return self.caches[key]
 
+    def print(self, message):
+        self.history.append(f"echo \"{message}\"")
+
 class TestFlow(unittest.TestCase):
-    def test_flow_start_1(self):
+    def test_flow_init_1(self):
         mock = ProcessMock()
-        flow = Flow(mock.exec)
+        mock.default("git branch --list base_branch", "* base_branch")
+        flow = Flow(mock.exec, mock.print)
 
         flow_name = "sample/1234"
         base_branch = "base_branch"
-        flow.start(flow_name, base_branch)
+        flow.init(flow_name, base_branch)
 
         commands = [
             'echo "sample/1234" > .git/flow_current',
@@ -40,12 +44,13 @@ class TestFlow(unittest.TestCase):
         self.assertEqual(history[0], commands[0])
         self.assertEqual(history[1], commands[1])
 
-    def test_flow_start_2(self):
+    def test_flow_init_2(self):
         mock = ProcessMock()
-        flow = Flow(mock.exec)
+        mock.default("git branch --list develop/sample/1234", "* develop/sample/1234")
+        flow = Flow(mock.exec, mock.print)
 
         flow_name = "sample/1234"
-        flow.start(flow_name)
+        flow.init(flow_name)
 
         commands = [
             'echo "sample/1234" > .git/flow_current',
@@ -56,12 +61,48 @@ class TestFlow(unittest.TestCase):
         self.assertEqual(history[0], commands[0])
         self.assertEqual(history[1], commands[1])
 
+    def test_flow_init_3(self):
+        mock = ProcessMock()
+        mock.default("git branch --list develop/sample/1234", "")
+        flow = Flow(mock.exec, mock.print)
+
+        flow_name = "sample/1234"
+        flow.init(flow_name)
+
+        command = "git checkout -b develop/sample/1234"
+        history = mock.get("git checkout")
+        self.assertEqual(len(history), 1)
+        self.assertEqual(history[0], command)
+
+        commands = [
+            'echo "sample/1234" > .git/flow_current',
+            'echo "develop/sample/1234" > .git/flow/sample_1234'
+        ]
+        history = mock.get("echo")
+        self.assertEqual(len(history), 2)
+        self.assertEqual(history[0], commands[0])
+        self.assertEqual(history[1], commands[1])
+
+    def test_start(self):
+        mock = ProcessMock()
+        mock.default("cat .git/flow_current", "sample/1234")
+        mock.default("cat .git/flow/sample_1234", "base_branch")
+        flow = Flow(mock.exec, mock.print)
+
+        flow_name = "sample/1234"
+        flow.start(flow_name)
+
+        command = 'echo "sample/1234" > .git/flow_current'
+        history = mock.get("echo")
+        self.assertEqual(len(history), 1)
+        self.assertEqual(history[0], command)
+
     def test_flow_checkout_1(self):
         mock = ProcessMock()
         mock.default("cat .git/flow_current", "sample/1234")
         mock.default("cat .git/flow/sample_1234", "base_branch")
-        mock.default("git show-ref", False)
-        flow = Flow(mock.exec)
+        mock.default("git branch --list feature/sample/1234/new", "")
+        flow = Flow(mock.exec, mock.print)
 
         feature_name = "new"
         flow.checkout(feature_name)
@@ -75,8 +116,8 @@ class TestFlow(unittest.TestCase):
         mock = ProcessMock()
         mock.default("cat .git/flow_current", "sample/1234")
         mock.default("cat .git/flow/sample_1234", "base_branch")
-        mock.default("git show-ref --quiet refs/heads/feature/sample/1234/new", True)
-        flow = Flow(mock.exec)
+        mock.default("git branch --list feature/sample/1234/new", "* feature/sample/1234/new")
+        flow = Flow(mock.exec, mock.print)
 
         feature_name = "new"
         flow.checkout(feature_name)
@@ -90,7 +131,7 @@ class TestFlow(unittest.TestCase):
         mock = ProcessMock()
         mock.default("cat .git/flow_current", "sample/1234")
         mock.default("cat .git/flow/sample_1234", "base_branch")
-        flow = Flow(mock.exec)
+        flow = Flow(mock.exec, mock.print)
 
         flow.checkout()
 
@@ -101,8 +142,8 @@ class TestFlow(unittest.TestCase):
     
     def test_flow_exists_1(self):
         mock = ProcessMock()
-        mock.default("git show-ref --quiet refs/heads/feature/sample/1234/new", True)
-        flow = Flow(mock.exec)
+        mock.default("git branch --list feature/sample/1234/new", "* feature/sample/1234/new")
+        flow = Flow(mock.exec, mock.print)
 
         branch = "feature/sample/1234/new"
         exists = flow.exists(branch)
@@ -111,8 +152,8 @@ class TestFlow(unittest.TestCase):
 
     def test_flow_exists_2(self):
         mock = ProcessMock()
-        mock.default("git show-ref --quiet refs/heads/feature/sample/1234/new", False)
-        flow = Flow(mock.exec)
+        mock.default("git branch --list feature/sample/1234/new", "")
+        flow = Flow(mock.exec, mock.print)
 
         branch = "feature/sample/1234/new"
         exists = flow.exists(branch)
@@ -123,7 +164,7 @@ class TestFlow(unittest.TestCase):
         mock = ProcessMock()
         mock.default("cat .git/flow_current", "sample/1234")
         mock.default("cat .git/flow_current_feature", "new")
-        flow = Flow(mock.exec)
+        flow = Flow(mock.exec, mock.print)
 
         flow.push()
 
@@ -136,7 +177,7 @@ class TestFlow(unittest.TestCase):
         mock = ProcessMock()
         mock.default("cat .git/flow_current", "sample/1234")
         mock.default("cat .git/flow/sample_1234", "base_branch")
-        flow = Flow(mock.exec)
+        flow = Flow(mock.exec, mock.print)
 
         flow.rebase()
 
@@ -153,7 +194,7 @@ class TestFlow(unittest.TestCase):
         mock = ProcessMock()
         mock.default("cat .git/flow_current", "sample/1234")
         mock.default("git branch --list 'feature/sample/1234/*' | tr '*' ' '", " feature/sample/1234/new\n feature/sample/1234/old")
-        flow = Flow(mock.exec)
+        flow = Flow(mock.exec, mock.print)
 
         flow.list()
 
